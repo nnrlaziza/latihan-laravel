b34c810ef79669da8c735b0d5bb1d0e232982586
lukluu <laode.lukmana88@gmail.com>
2024-01-31 02:17:35 +0700

    category

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -8,7 +8,8 @@
         @foreach ($category as $category)
 
         <div class="col-md-4">
-            <a href="/categories/{{ $category->id }}">
+
+            <a href="/categories/{{ $category->slug}}">
                 <div class="card bg-dark text-white">
                     <img src="https://source.unsplash.com/400x400?{{ $category->name }}" class="card-img-top" alt="{{ $category->name }}">
                     <div class="card-img-overlay d-flex align-items-center p-0">
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,9 @@
 
 @section('container')
 <article>
-<h2>{{ $post->title }}</h2>
+
+<h1 class="mb-5">{{ $post->title }}</h2>
+<p>By. {{ $post->author->name }} in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }} </a></p>
 <p>{!! $post->body !!}</p>
 </article>
 <a href="/posts">Back to Posts</a>
